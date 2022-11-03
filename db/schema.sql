121e58d1043024d05fe8d68a65a959f325eafb2a
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-11-02 20:49:17 -0400

    created schema file

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    depName VARCHAR(30) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id)
+    REFERENCES departments (id) 
+);
+
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employees (id)
+);
+
